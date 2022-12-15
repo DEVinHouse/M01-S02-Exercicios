656deaa84df39f4022fcb45c29c167b939c9db8c
Nayane Mittelstaedt <nayanemittelstaedt@gmail.com>
2022-12-15 01:52:22 -0300

    Exercício 4

---
+++ Ex4/index.js
@@ -0,0 +1,14 @@
+var soma = 0
+
+do {
+    
+    var numero = parseInt(prompt('Digite um numero: '));
+
+    if ( isNaN(numero) || numero === -1 ){
+        continue;
+    }
+    soma += numero;
+
+} while (numero !== -1);
+
+alert(soma);
